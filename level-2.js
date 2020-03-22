427379651f22842121705bdbe660363a13a13ced
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2020-03-22 13:39:26 +0100

    Add files via upload

---
+++ level-2.js
@@ -0,0 +1,45 @@
+// 1. Using the loop from question 5 above, only log the counter variable if it is an even number.
+
+// Hint: you can use the modulus % operator for this.
+
+
+for (var i = 15; i <= 25; i++) {
+
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+/* 2. Create a function that logs the sentence "I am a function".
+Assign this function to a variable called innerFunction.
+Create another function called outerFunction that accepts one argument.
+Inside outerFunction, call the argument like you would a function - with parenthesis ().
+Call outerFunction and pass in the innerFunction variable. */
+
+
+
+
+function firstFunction(iAmFunction) {
+
+    console.log(iAmFunction);
+
+}
+
+
+var iAmFunction = "I ma a funcrtion";
+
+var innerFunction = firstFunction(iAmFunction);
+
+
+function outerFunction(oneArgument) {
+    oneArgument();
+}
+
+
+outerFunction(innerFunction);
